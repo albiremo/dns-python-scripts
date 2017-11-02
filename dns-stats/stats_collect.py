a7ea5197690eb451f787319229651e91f0232047
albiremo <albertoremigi92@gmail.com>
2017-11-02 16:02:48 +0100

    bug fixes

--- dns-stats/stats_collect.py
+++ dns-stats/stats_collect.py
@@ -7,6 +7,7 @@ utau=1.00101;
 y_ret = np.tanh(a*(2*np.arange(-1,ny+2)/(1.0*ny)-1))/np.tanh(a)+1
 yplus=y_ret*1000.0*utau;
 databis = np.zeros([(ny+3)*5+1] , dtype=np.float64)
+datatemp = np.zeros([(ny+3)*5+1] , dtype=np.float64)
 direct = range(2,15)
 for i in direct:
     h = str(i)
@@ -19,12 +20,14 @@ for i in direct:
       # Read data
       print(files[i])
       data = np.fromfile(files[i],count=((ny+3)*5+1))
-      databis += data
-    databis = databis / num
+      datatemp += data
+    datatemp = datatemp / num
+    databis += datatemp
+    datatemp = np.zeros([(ny+3)*5+1], dtype=np.float64)
     os.chdir('../')
+databis = databis / len(direct)
 statist = open('stat_bin.dat', "wb")
 statist.write(bytes(databis))
-
 data_save = databis[1:].reshape(5,ny+3)
 with open('stat_ASCII.dat',"w") as stat_file:
     for (i,j) in [(i,j) for j in  range(0,ny+3)]:
